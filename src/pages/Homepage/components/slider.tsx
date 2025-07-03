7143180579930a457f28025f4aaddee004a97aea
Lucas Almeida <lucasrodolfo912@gmail.com>
2025-07-03 02:31:30 -0300

    Added: Checking if the user is on mobile

--- src/pages/Homepage/components/slider.tsx
+++ src/pages/Homepage/components/slider.tsx
@@ -1,23 +1,39 @@
+import { useEffect, useState } from "react";
+
 interface ISliderProps {
   $onSetCategory: (value: "player" | "goalkeeper") => void;
 }
 
 export function Slider({ $onSetCategory }: ISliderProps) {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+
+    if (/Android/i.test(userAgent) || /Ipad|iPhone|iPod/.test(userAgent)) {
+      setIsMobile(true);
+    }
+  }, []);
+
   return (
     <div className="w-[300px] overflow-x-auto scroll-smooth">
-      <style>
-        {`
-          div::-webkit-scrollbar {
-            width: 8px;
-          }
-          div::-webkit-scrollbar-track {
-            background: transparent;
-          }
-          div::-webkit-scrollbar-thumb {
-            background-color: transparent;
-          }
-        `}
-      </style>
+      {
+        isMobile && (
+          <style>
+            {`
+              div::-webkit-scrollbar {
+                width: 8px;
+              }
+              div::-webkit-scrollbar-track {
+                background: transparent;
+              }
+              div::-webkit-scrollbar-thumb {
+                background-color: transparent;
+              }
+            `}
+          </style>
+        )
+      }
 
       <div className="flex space-x-4 px-2">
         <div className="min-w-[240px] max-w-[240px] min-h-[260px] rounded-md bg-[url(/images/player.png)] bg-cover">
