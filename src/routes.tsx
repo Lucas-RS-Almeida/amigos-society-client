03a82b220db54bc68279c1a44d0a31e93e4c8031
Lucas Almeida <lucasrodolfo912@gmail.com>
2025-07-05 02:29:05 -0300

    update: check authentication to realise route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,22 @@
+import { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
+
+import { Context } from "./contexts/AuthContext";
 
 import { Homepage } from "./pages/Homepage";
 import { Authentication } from "./pages/Authentication";
 import { Match } from "./pages/Match";
 
 export function CustomRoutes() {
+  const { isAuthenticated } = useContext(Context);
+
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/authentication" element={<Authentication />} />
-      <Route path="/matchs" element={<Match />} />
+      {!isAuthenticated && (
+        <Route path="/authentication" element={<Authentication />} />
+      )}
+      <Route path="/matches" element={<Match />} />
     </Routes>
   );
 }
