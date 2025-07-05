01c3d3c002bedc83bb458f13c222465b9aa4a3fd
Lucas Almeida <lucasrodolfo912@gmail.com>
2025-07-05 02:28:26 -0300

    update: function to check user token with useEffect

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,12 @@
-import { createContext, useState, type ReactNode } from "react";
+import {
+  createContext,
+  useEffect,
+  useState,
+  type ReactNode,
+ } from "react";
+import { useMatch, useNavigate } from "react-router-dom";
 import type { AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 
 import { api } from "../api";
 
@@ -31,18 +38,60 @@ export function AuthProvider({ children }: IAPProps) {
   const [username, setUsername] = useState<string>("");
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function checkUser() {
+      const tokenLS = localStorage.getItem("@amigos_society_token");
+      const usernameLS = localStorage.getItem("@amigos_society_username");
+      const tokenParse = tokenLS ? JSON.parse(tokenLS) : "";
+      const usernameParse = usernameLS ? JSON.parse(usernameLS) : "";
+
+      if (tokenParse) {
+        try {
+          await api.get("/auth/check-user", {
+            headers: {
+              Authorization: `Bearer ${tokenParse}`,
+            },
+          });
+
+          api.defaults.headers.common["Authorization"] = `Bearer ${tokenParse}`;
+
+          setIsAuthenticated(true);
+          setUsername(usernameParse);
+
+          navigate("/matches");
+        } catch (error: any) {
+          toast.error(error?.response?.data?.error);
+
+          navigate("/authentication");
+
+          localStorage.removeItem("@amigos_society_token");
+          localStorage.removeItem("@amigos_society_username");
+        }
+      }
+    }
+
+    checkUser();
+  }, []);
+
   async function onLogin(body: IBodyAuthProps) {
     try {
       const response: AxiosResponse<IResponseProps> = await api
         .post("/auth/log-in", body);
 
-      api.defaults.headers
-        .common["Authorization"] = `Bearer ${response.data.token}`;
+      const { user, token } = response.data;
 
-      setUsername(response.data.user.username);
+      localStorage.setItem("@amigos_society_token", JSON.stringify(token));
+      localStorage.setItem("@amigos_society_username", JSON.stringify(user.username));
+
+      api.defaults.headers
+        .common["Authorization"] = `Bearer ${token}`;
+
+      setUsername(user.username);
       setIsAuthenticated(true);
     } catch (error: any) {
-      throw new Error(error?.response?.data?.error);
+      throw new Error(error?.response?.data?.error || error?.msessage);
     }
   }
 
