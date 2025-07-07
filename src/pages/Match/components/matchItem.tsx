510f58b505a21dbf4e3bb1be1fbe9dfec9f7867a
Lucas Almeida <lucasalmeida.devjs@gmail.com>
2025-07-07 12:26:31 -0300

    update: get score implemented

--- src/pages/Match/components/matchItem.tsx
+++ src/pages/Match/components/matchItem.tsx
@@ -1,12 +1,60 @@
+import { useState, useEffect } from "react";
+import type { AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 import { IoClose, IoChevronDown } from "react-icons/io5";
 
 import type { IMatchProps } from "./matches";
+
+import { api } from "../../../api";
 
 interface IMIProps {
   match: IMatchProps;
 }
 
+export interface IMPProps {
+  match_player: {
+    id: string,
+    matchId: string,
+    playerId: string,
+    teamId: string,
+    goals: number,
+    yellowCards: number,
+    redCard: boolean,
+    createdAt: string,
+    updatedAt: string,
+  },
+  match: {
+    id: string,
+    homeTeamId: string,
+    awayTeamId: string,
+    homeScore: number,
+    awayScore: number,
+    matchDay: string,
+    inProgress: boolean,
+    createdAt: string,
+    updatedAt: string,
+  },
+  player: {
+    id: string,
+    name: string,
+    playerType: string,
+    teamId: string,
+    matchDay: string,
+    ipPlayer: string,
+    createdAt: string,
+    updatedAt: string,
+  },
+  team: {
+    id: string,
+    name: string,
+    createdAt: string,
+    updatedAt: string,
+  }
+}
+
 export function MatchItem({ match }: IMIProps) {
+  const [matchPlayer, setMatchPlayer] = useState<IMPProps[]>([]);
+
   const colorsTeams = [
     {
       name: "Amarelo",
@@ -28,7 +76,22 @@ export function MatchItem({ match }: IMIProps) {
       name: "Preto",
       color: "#000",
     },
-  ]
+  ];
+
+  useEffect(() => {
+    async function loadMatchPlayer() {
+      try {
+        const response: AxiosResponse<IMPProps[]> = await api
+          .get(`/match-player/${match.matches.id}`);
+
+        setMatchPlayer(response.data);
+      } catch (error: any) {
+        toast.error(error?.response?.data?.error);
+      }
+    }
+
+    loadMatchPlayer();
+  }, []);
 
   return (
     <li className="p-4 rounded-md bg-[rgba(0,0,0,0.4)]">
@@ -54,7 +117,10 @@ export function MatchItem({ match }: IMIProps) {
           </div>
 
           <span className="text-5xl">
-            {match.matches.homeScore}
+            {
+              matchPlayer.find((mp) => mp.match_player.teamId === match.home_team.id)
+                ?.match_player.goals || 0
+            }
           </span>
         </div>
 
@@ -62,7 +128,10 @@ export function MatchItem({ match }: IMIProps) {
 
         <div className="flex items-start gap-12">
           <span className="text-5xl">
-            {match.matches.awayScore}
+            {
+              matchPlayer.find((mp) => mp.match_player.teamId === match.away_team.id)
+                ?.match_player.goals || 0
+            }
           </span>
 
           <div className="flex flex-col items-end">
