17b459f45530c37ffbdb1103fff0b054a60dbf38
Lucas Almeida <lucasrodolfo912@gmail.com>
2025-07-11 23:34:47 -0300

    update: card no content implemented

--- src/pages/Match/components/table.tsx
+++ src/pages/Match/components/table.tsx
@@ -1,7 +1,14 @@
+import { FaTable } from "react-icons/fa";
+import { NoContentCard } from "../../../components/noContentCard";
+
 export function Table() {
   return (
     <div>
-      Table
+      <NoContentCard
+        $icon={<FaTable className="text-5xl" />}
+        $message="Nenhum dado pra gerar tabela, insira partidas."
+        $isAction={false}
+      />
     </div>
   )
 }
