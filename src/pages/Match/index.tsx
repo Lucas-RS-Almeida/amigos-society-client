66fcf5e3e335a4dbe6de1eb1b9b17411bbfbb5e7
Lucas Almeida <lucasrodolfo912@gmail.com>
2025-07-11 23:35:15 -0300

    update: page draw team implemented in to menu

--- src/pages/Match/index.tsx
+++ src/pages/Match/index.tsx
@@ -4,11 +4,21 @@ import { CgMenuRight } from "react-icons/cg";
 import { Matches } from "./components/matches";
 import { Players } from "./components/players";
 import { Table } from "./components/table";
+import { Menu } from "../../components/menu";
+import { DrawTeam } from "./components/drawTeam";
 
-type TCTProps = "matches" | "players" | "table";
+export type TCTProps = "matches" | "players" | "table" | "draw";
 
 export function Match() {
   const [contentView, setContentView] = useState<TCTProps>("matches");
+
+  const [menuIsVisible, setMenuIsVisible] = useState<boolean>(false);
+
+  function handleToggleMenu(contentName: TCTProps) {
+    setContentView(contentName);
+
+    setMenuIsVisible(false);
+  }
 
   return (
     <div className="min-h-screen p-4 relative">
@@ -19,16 +29,23 @@ export function Match() {
           className="w-full max-w-[40px]"
         />
 
-        <button>
+        <button onClick={() => setMenuIsVisible(true)}>
           <CgMenuRight className="text-2xl text-[#cfa321]" />
         </button>
       </header>
+
+      <Menu
+        $isVisible={menuIsVisible}
+        $onToggleMenu={handleToggleMenu}
+      />
 
       {
         contentView === "matches"
           ? <Matches />
           : contentView === "players"
           ? <Players />
+          : contentView === "draw"
+          ? <DrawTeam />
           : <Table />
       }
     </div>
